1a6dba478e37b93f644dc4511701f2b2826ab371
Rac00nCanFly <139277025+Rac00nCanFly@users.noreply.github.com>
2025-06-10 19:36:28 +0200

    Modified dynamic priorities

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import task_manager as t
 def main():
-    choice = input("What action do you want to take? \n Add task \n Update task \n View tasks ")
+    choice = input("What action do you want to take? \n 1. Add task \n 2. Update task \n 3. View tasks \n 4. Set parameters \n Input number between 1-4")
 
     match choice:
-        case "Add task":
+        case 1:
             t.addtask()
 
-        case "Update task":
+        case 2:
             t.updatetask()
 
-        case "View tasks":
+        case 3:
             t.viewtasks()
+        case 4:
+            t.setting_parameters()
         case _:
             print("No option found")
 
--- task_manager.py
+++ task_manager.py
@@ -2,6 +2,7 @@ import json
 import os
 from datetime import datetime
 from operator import itemgetter
+import math
 def load_tasks():
     if os.path.exists('tasks.json'):
         with open('tasks.json', 'r', encoding='utf-8') as file:
@@ -10,6 +11,7 @@ def load_tasks():
 def addtask():
     tasks = load_tasks()
     title = input("Task name:")
+    category = input("Category of the task:")
     while True:
         deadline = input("What is the due date?(Put it in format dd-mm-rrrr)")
         try:
@@ -21,8 +23,10 @@ def addtask():
         status = input("What is the status of this task?(Not started,In progress, Finished):")
         status_progress = ["Not started","In progress", "Finished"]
         try:
-            if status in status_progress:
+            if status == "Not started" or status == "In progress":
                 break
+            elif status == 'Finished':
+                completed_at = input("When did you completed it?")
         except:
             print("Wrong status, try again")
     while True:
@@ -35,7 +39,8 @@ def addtask():
         except ValueError:
             print("Please enter a valid integer.")
     task_id = max([task["id"] for task in tasks], default=0) + 1
-    task = {"id": task_id, "title": title, "deadline": deadline, "status": status, "priority":priority}
+    created_at = datetime.now().isoformat()
+    task = {"id": task_id,"category":category, "title": title, "deadline": deadline, "status": status, "priority":priority, "created_at":created_at}
     tasks.append(task)
     tasks.sort(key=lambda t: (t['priority'],datetime.strptime(t['deadline'], "%d-%m-%Y")))
     with open('tasks.json', 'w', encoding='utf-8') as file:
@@ -50,10 +55,25 @@ def viewtasks():
             return
         dane.sort(key=lambda t: (t['priority'], datetime.strptime(t['deadline'], "%d-%m-%Y")))
         for task in dane:
-            print(f"ID: {task['id']} | Tytuł: {task['title']} | Termin: {task['deadline']} | Status: {task['status']} | Priorytet: {task['priority']}")
+            print(f"ID: {task['id']} | Category:{task['category']}| Tytuł: {task['title']} | Termin: {task['deadline']} | Status: {task['status']} | Priorytet: {task['priority']}")
     except FileNotFoundError:
         print("No file found")
 
-    
+def update_priority(tasks):
+    today = datetime.now()
+    for task in tasks:
+        created_at = datetime.fromisoformat(task['created_at'])
+        days_waiting = (today - created_at).days
+        new_priority = min(max_priority, int(min_priority + K *math.log(days_waiting+1)))
+    print(days_left)
+def setting_parameters():
+    max_days = input("What is the maximum number of days after which task should be completed?(e.g.: usually you input tasks in 14 days advance their deadline)")
+    min_days = input("What is the minimum time after which the task should be marked as high priority?")
+    max_priority = 100
+    min_priority = 1
+    K = (max_priority-min_priority)/(math.log(max_days+1))
+def saving_history_for_ML():
+
 def updatetask():
-    pass
+    if status == 'Finished':
+        completion_time = input("How much time did it take to complete this task?(Input in hours)")
